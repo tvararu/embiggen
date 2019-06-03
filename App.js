7c3baeeeb1f4952f4972989a82d7cbb739298e8f
Theodor Vararu <theo@vararu.org>
2019-06-03 14:02:28 +0100

    Use a sfc for App.js

--- App.js
+++ App.js
@@ -1,15 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Hello world!</Text>
-      </View>
-    );
-  }
-}
+export default () => (
+  <View style={styles.container}>
+    <Text style={styles.text}>Hello world!</Text>
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
