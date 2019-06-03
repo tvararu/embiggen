b7153cb9f8f48edbe73387febdbb1300f8b82627
Theodor Vararu <theo@vararu.org>
2019-06-03 14:01:03 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Hello world!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  text: {
+    fontSize: 50
+  }
+});
